1da20f91846c369ed8a8aea1470fa5bcad27956e
AntonyRyanF07 <ryan.antony10@aluno.ifce.edu.br>
2026-06-16 18:06:30 -0300

    Update UsuarioInvalidoException.java

--- src/main/java/com/biblioteca/model/exceptions/UsuarioInvalidoException.java
+++ src/main/java/com/biblioteca/model/exceptions/UsuarioInvalidoException.java
@@ -1,6 +1,6 @@
 package com.biblioteca.model.exceptions;
 
-public class UsuarioInvalidoException extends Exception {
+public class UsuarioInvalidoException extends RuntimeException {
     public UsuarioInvalidoException(String mensagem) {
         super(mensagem);
     }
