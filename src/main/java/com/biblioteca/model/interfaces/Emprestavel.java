4d2ff0a6ef8fdddcb08df393de824d93e5aa03eb
AntonyRyanF07 <ryan.antony10@aluno.ifce.edu.br>
2026-06-16 17:59:00 -0300

    Update Emprestavel.java

--- src/main/java/com/biblioteca/model/interfaces/Emprestavel.java
+++ src/main/java/com/biblioteca/model/interfaces/Emprestavel.java
@@ -3,7 +3,7 @@ package com.biblioteca.model.interfaces;
 import com.biblioteca.model.exceptions.LivroJaEmprestadoException;
 
 public interface Emprestavel {
-    void emprestar() throws LivroJaEmprestadoException;
+    void emprestar();
     void devolver();
     boolean estaEmprestado();
 }
